c4ffc171eb2cf38256b950d90650d33a8f79a814
Rafael <rafaeltomaz2701@gmail.com>
2026-03-31 02:20:27 -0300

    DialogBoxDone

--- src/components/DialogBox.jsx
+++ src/components/DialogBox.jsx
@@ -3,23 +3,40 @@
 import styles from './DialogBox.module.scss'
 import { useState } from 'react';
 import { useEffect } from 'react';
+import { useRef } from 'react';
 
 export function DialogBox() {
-    const [text, setText] = useState("Ola Mundo");
-    const arrayTexto = ["paragrafo 1", "paragrafo 2", "paragrafo 3", "paragrafo 4"]
+    const [index, setIndex] = useState(0)
+    const arrayTexto = ["paaragrafo 1", "paaragrafo 2", "paaragrafo 3", "paaragrafo 4"]
+    const arrayImagens = ["/images/rafael_sorrindo.png", "/images/rafael_soprando.png", "/images/rafael_nerd.png", "/images/rafael_timido.png"]
+    const [text, setText] = useState(arrayTexto[0]);
+    const [image, setImage] = useState(arrayImagens[0])
+
+    function changeText() {
+        if (index == 3) {
+            setIndex(0);
+        } else {
+            setIndex(index + 1)
+        }
+
+        setImage(arrayImagens[index])
+        setText(arrayTexto[index])
+    }
 
     function Typewriter({ texto, velocidade = 50 }) {
         const [conteudo, setConteudo] = useState("");
 
+        const indexRef = useRef(0);
+
         useEffect(() => {
-            let i = 0;
+            indexRef.current = 0;
             setConteudo("");
 
             const interval = setInterval(() => {
-                setConteudo((prev) => prev + texto.charAt(i));
-                i++;
+                setConteudo((prev) => prev + texto.charAt(indexRef.current));
+                indexRef.current++;
 
-                if (i >= texto.length) {
+                if (indexRef.current >= texto.length) {
                     clearInterval(interval);
                 }
             }, velocidade);
@@ -32,12 +49,12 @@ export function DialogBox() {
 
     return (
         <div className={`${styles.dialog__container} row align-items-center justify-content-center`}>
-            <div className={`col-10 col-sm-8`} onClick={() => setText("teste")}>
+            <div className={`col-10 col-sm-8`} onClick={changeText}>
                 <section className={`${styles.dialog} row`}>
-                    <img className={`${styles.dialog__img} col-12 col-sm-4`} src="https://i.pinimg.com/564x/b3/cf/53/b3cf53b1b2ba150b49030eacc11ec146.jpg" alt="" />
-                    
+                    <img className={`${styles.dialog__img} col-12 col-sm-4`} src={image} alt="" />
+
                     <div className={`${styles.dialog__text} col-12 col-sm-8`}>
-                        <Typewriter texto={text} />  
+                        <Typewriter texto={text} />
                     </div>
                 </section>
             </div>
